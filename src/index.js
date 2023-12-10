b2827804e3804e0a2a111ab2eebbde6f7fca431c
Pohrebniak Andrian <pogra29@gmail.com>
2023-12-10 16:50:15 +0200

    ff

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import { fetchinfo, createMarkup } from './js/query-pixa';
 import { Notify } from 'notiflix';
 const loadMore = document.querySelector('.load-more');
 loadMore.classList.add('is-hidden');
-
 // const search = document.getElementById('search-form');
 // const gallery = document.querySelector('.gallery');
-
 const objectInfo = {
   search: document.getElementById('search-form'),
   gallery: document.querySelector('.gallery'),
 };
-export { objectInfo }; 
+export { objectInfo };
 const per_page = 40;
 let page = 1;
 let searchValue = '';
@@ -18,14 +16,14 @@ objectInfo.search.addEventListener('submit', onFetch);
 
 function onFetch(evt) {
   evt.preventDefault();
-  objectInfo.gallery.innerHTML = ""; 
+  objectInfo.gallery.innerHTML = '';
   searchValue = evt.target.searchQuery.value;
   searchValue = searchValue.toLowerCase().trim();
   if (searchValue === '') {
     Notify.info('Please write queary!');
     return;
   }
-  page = 1; 
+  page = 1;
   fetchinfo(searchValue, page, per_page)
     .then(data => {
       let datasearch = data.hits;
@@ -34,25 +32,41 @@ function onFetch(evt) {
           'Sorry, there are no images matching your search query. Please try again.'
         );
         loadMore.classList.add('is-hidden');
-        evt.target.reset(); 
-        return
+        evt.target.reset();
+        return;
       } else {
         Notify.success(`Hooray! We found ${data.total} totalHits images.`);
         createMarkup(datasearch);
-        loadMore.classList.remove("is-hidden"); 
+        loadMore.classList.remove('is-hidden');
       }
-      loadMore.addEventListener('click', LoadingMore); 
-      evt.target.reset(); 
+      if (data.total < per_page) { 
+        loadMore.classList.add('is-hidden')
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        return
+      }
     })
-    .catch(error => { 
-      throw new Error(`${error}`); 
-  })
+    .catch(error => {
+      throw new Error(`${error}`);
+    });
+  loadMore.addEventListener('click', LoadingMore);
+  evt.target.reset();
 }
+
+
 function LoadingMore() {
-  page += 1; 
-  fetchinfo(searchValue, page, per_page)
-    .then(data => { 
-      datasearch = data.hits;
-      createMarkup(datasearch);    
-  })
+  page += 1;
+  console.log(`page = ${page}`);
+  fetchinfo(searchValue, page, per_page).then(data => {
+    datasearch = data.hits;
+    const lastPage = Math.ceil(data.totalHits / per_page);
+    console.log(lastPage);
+    if (page === lastPage + 1 || data.totalHits < per_page) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      loadMore.classList.add('is-hidden');
+      return;
+    }
+    createMarkup(datasearch);
+  });
 }
