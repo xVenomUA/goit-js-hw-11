497f702792d3dc241b07beda02e633e4418bf34b
Pohrebniak Andrian <pogra29@gmail.com>
2023-12-10 12:08:31 +0200

    Ff

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
-import { fetchinfo } from "./js/query-pixa";
-import Notiflix from "notiflix";
-import simpleLightbox from "simplelightbox";
-import 'simplelightbox/dist/simple-lightbox.min.css'; 
+import { fetchinfo, createMarkup } from './js/query-pixa';
+import { Notify } from 'notiflix';
 const loadMore = document.querySelector('.load-more');
-loadMore.setAttribute('hidden', true); 
+loadMore.setAttribute('hidden', true);
 
-const search = document.getElementById('search-form');
+// const search = document.getElementById('search-form');
+// const gallery = document.querySelector('.gallery');
 
-const per_page = 40; 
-let page = 1; 
+const objectInfo = {
+  search: document.getElementById('search-form'),
+  gallery: document.querySelector('.gallery'),
+};
+export { objectInfo }; 
+const per_page = 40;
+let page = 1;
 let searchValue = '';
-search.addEventListener('submit', onFetch); 
+objectInfo.search.addEventListener('submit', onFetch);
 
 function onFetch(evt) {
-  evt.preventDefault(); 
-  searchValue = evt.target.searchQuery.value; 
-  console.log(searchValue);
+  evt.preventDefault();
+  objectInfo.gallery.innerHTML = ""; 
+  searchValue = evt.target.searchQuery.value;
+  searchValue = searchValue.toLowerCase().trim();
+  if (searchValue === '') {
+    Notify.failure('Please write queary!');
+    return;
+  }
+  page = 1; 
+  fetchinfo(searchValue, page, per_page)
+    .then(data => {
+      let datasearch = data.hits;
+      if (data.total === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return
+      } else {
+        Notify.success(`Hooray! We found ${data.total} totalHits images.`);
+        createMarkup(datasearch);
+      }
+    })
+    .catch(error => { 
+      throw new Error(`${error}`); 
+  })
 }
-
-
-
-
--- src/js/query-pixa.js
+++ src/js/query-pixa.js
@@ -1,12 +1,45 @@
 import axios from "axios";
-
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { objectInfo } from "../index";
 const Api_key = '40756763-1e424adc67840c21112a3a2f8';
 const base_url = 'https://pixabay.com/api/';
 
 async function fetchinfo(q, page, per_page) { 
     const response = await axios.get(
-      `${base_url}/?key=${Api_key}&=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`
+      `${base_url}/?key=${Api_key}&&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`
     );
     return response.data;
 }
-export { fetchinfo }; 
+
+function createMarkup(datasearch) { 
+  const array = datasearch.map(
+    ({
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {
+      return `<div class="photo-card"><a class="gallery_link" href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      </a><div class="info"><p class="info-item"><b>Likes: ${likes}</b></p><p class="info-item"><b>Views: ${views}</b></p><p class="info-item"><b>Comments: ${comments}</b></p><p class="info-item"><b>Downloads: ${downloads}</b></p></div></div>`;
+    }
+  ).join("");
+  objectInfo.gallery.innerHTML += array; 
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+
+}
+
+
+
+
+
+
+
+export { fetchinfo, createMarkup }; 
+
